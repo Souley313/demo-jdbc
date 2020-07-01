353f115941eee09a9c258256ed684e136df63f63
Souleymane THIAM <souleymanethiam22@gmail.com>
2020-07-01 15:05:11 +0200

    JDBC–TPN°03–OperationsCRUD.EXO2

---
+++ src/main/java/fr/diginamic/jdbc/TestUpdate.java
@@ -0,0 +1,42 @@
+package fr.diginamic.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class TestUpdate {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+
+		ResourceBundle database = ResourceBundle.getBundle("database");
+
+		Class.forName(database.getString("database.driver"));
+
+		// Etape 2 - Créer une connexion
+		String url = database.getString("database.url"); // url JDBC d'accès à la base (machine, port, le nom de la
+															// base, le type de base...)
+		String utilisateur = database.getString("database.user");
+		String motDePasse = database.getString("database.pass");
+
+		try (Connection uneConnexion = DriverManager.getConnection(url, utilisateur, motDePasse)) {
+
+			uneConnexion.setAutoCommit(false);
+
+			try (Statement statement = uneConnexion.createStatement();
+					ResultSet resultSet = statement.executeQuery("select * from  FOURNISSEUR");) {
+
+				int nbLignesImpactees = statement.executeUpdate("update FOURNISSEUR set nom= 'La Maison de la Peinture' where id = 4");
+
+				uneConnexion.commit();
+			} catch (SQLException e) {
+				uneConnexion.rollback();
+			}
+		}
+
+	}
+
+}
